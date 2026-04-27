fb78d4d56db0a3aca959408ff458557732f049c7
PipGriffiths <griffiths.pip@gmail.com>
2026-04-27 23:23:49 +0100

    Specify numpy version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "numpy",
+        "numpy == 1.26.4",
         "scipy",
         "scikit-image",
         "matplotlib",
